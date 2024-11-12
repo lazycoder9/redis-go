642400cf95e7ed55547a24c1a46b7a385f132db2
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2024-11-12 16:24:30 +0500

    Try to re-run tests

--- app/server.go
+++ app/server.go
@@ -7,42 +7,41 @@ import (
 	"strings"
 )
 
-func handleRequest(conn net.Conn) {
-	defer conn.Close()
-
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-
-	if err != nil {
-		fmt.Println("Error reading: ", err.Error())
-		return
-	}
-
-	data := string(buf[:n])
-
-	switch {
-	case strings.Contains(data, "PING"):
-		conn.Write([]byte("+PONG\r\n"))
-	default:
-		conn.Write([]byte("-ERR unknown command\r\n"))
-	}
-}
-
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	for {
-		conn, err := l.Accept()
 
+	defer listener.Close()
+
+	conn, err := listener.Accept()
+
+	for {
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
-		go handleRequest(conn)
-	}
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
 
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			fmt.Println("Error reading: ", err.Error())
+			return
+		}
+		data := string(buf[:n])
+
+		switch {
+		case strings.Contains(data, "PING"):
+			conn.Write([]byte("+PONG\r\n"))
+		default:
+			conn.Write([]byte("-ERR unknown command\r\n"))
+		}
+	}
 }
