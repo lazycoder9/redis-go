1586362251bf34869c56567d00a4551dc7c67dd5
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2024-11-12 16:14:04 +0500

    implement ping-pong

--- app/server.go
+++ app/server.go
@@ -4,26 +4,44 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		return
+	}
+
+	data := string(buf[:n])
+
+	switch {
+	case strings.Contains(data, "PING"):
+		conn.Write([]byte("+PONG\r\n"))
+	default:
+		conn.Write([]byte("-ERR unknown command\r\n"))
+	}
+}
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleRequest(conn)
 	}
+
 }
