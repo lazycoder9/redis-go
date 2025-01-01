ca855d7374544371bd3415fcd66bfe3e57578693
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2025-01-01 19:54:16 +0500

    Handle concurrent connections

--- app/server.go
+++ app/server.go
@@ -7,27 +7,10 @@ import (
 	"strings"
 )
 
-func main() {
-	listener, err := net.Listen("tcp", "0.0.0.0:6379")
-
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-
-	defer listener.Close()
-
-	conn, err := listener.Accept()
-
+func handleConnection(conn net.Conn) {
 	for {
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-
 		buf := make([]byte, 1024)
 		n, err := conn.Read(buf)
-
 		if err != nil {
 			if err.Error() == "EOF" {
 				break
@@ -43,5 +26,25 @@ func main() {
 		default:
 			conn.Write([]byte("-ERR unknown command\r\n"))
 		}
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 }
