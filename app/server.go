af1c750376851ece6b582127959265daf45210ab
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2025-01-03 23:20:29 +0500

    First try Redis protocol parser, handle ECHO

--- app/server.go
+++ app/server.go
@@ -4,8 +4,90 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"slices"
 	"strings"
 )
+
+type Response struct {
+	Type string
+	Data []byte
+}
+
+type Request struct {
+	Type   string
+	Raw    []byte
+	Conn   net.Conn
+	Fields []string
+}
+
+func (r *Response) buildResponseMessage() string {
+	return string(r.Data)
+}
+
+func (r *Response) getPrefix() string {
+	switch r.Type {
+	case "bulk":
+		return "$4"
+	default:
+		return "+"
+	}
+}
+
+func (r *Request) parseType() {
+	var requestType string
+	switch string(r.Raw[0]) {
+	case "$":
+		requestType = "bulk"
+	case "+":
+		requestType = "string"
+	case "*":
+		requestType = "array"
+	default:
+		requestType = ""
+	}
+
+	r.Type = requestType
+}
+
+func (r *Request) handleStringRequest() {
+	switch {
+	case strings.HasPrefix(string(r.Raw[1:]), "PING"):
+		r.Conn.Write([]byte("+PONG\r\n"))
+	default:
+		r.Conn.Write([]byte("-ERR unknown command\r\n"))
+	}
+}
+
+func (r *Request) handleArrayRequest() {
+	parts := slices.Collect(slices.Chunk(r.Fields[1:], 2))
+	command := parts[0][1]
+	switch strings.ToUpper(command) {
+	case "PING":
+		r.Conn.Write([]byte("+PONG\r\n"))
+	case "ECHO":
+		r.Conn.Write([]byte(strings.Join(parts[1], "\r\n") + "\r\n"))
+	default:
+		r.Conn.Write([]byte("-ERR unknown command\r\n"))
+	}
+}
+
+func makeRequest(conn net.Conn, rawData []byte) Request {
+	fields := strings.Fields(string(rawData))
+	req := Request{Conn: conn, Raw: rawData, Fields: fields}
+	return req
+}
+
+func handleRequest(r Request) {
+	fmt.Println(r.Fields)
+	switch r.Type {
+	case "string":
+		r.handleStringRequest()
+	case "array":
+		r.handleArrayRequest()
+	default:
+		r.Conn.Write([]byte("-ERR unknown command\r\n"))
+	}
+}
 
 func handleConnection(conn net.Conn) {
 	for {
@@ -18,14 +100,10 @@ func handleConnection(conn net.Conn) {
 			fmt.Println("Error reading: ", err.Error())
 			return
 		}
-		data := string(buf[:n])
-
-		switch {
-		case strings.Contains(data, "PING"):
-			conn.Write([]byte("+PONG\r\n"))
-		default:
-			conn.Write([]byte("-ERR unknown command\r\n"))
-		}
+		data := buf[:n]
+		request := makeRequest(conn, data)
+		request.parseType()
+		handleRequest(request)
 	}
 }
 
