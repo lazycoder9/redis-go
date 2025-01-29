664e4a02733bb02d9ad12fe8c584cafa58bde1d0
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2025-01-29 11:15:07 +0500

    Declare ParserState struct, refactor parser

--- app/parser.go
+++ app/parser.go
@@ -25,146 +25,150 @@ const (
 	stateArgValue      = "arg_value"      // Actual argument value
 )
 
-var (
-	validCommands = []string{"ECHO", "PING", "SET"}
-	ignoredTokens = []string{TokenCRLF, TokenBulkString}
-)
-
-var checkers map[string]ParserStateChecker = map[string]ParserStateChecker{
-	stateArrayHeader: func(p *Parser) error {
-		if p.tokens[p.pos].Type != TokenArray {
-			return fmt.Errorf("invalid: no array start")
-		}
-		return nil
-	},
-	stateCommandLength: func(p *Parser) error {
-		if p.tokens[p.pos].Type != TokenBulkString {
-			return fmt.Errorf("invalid: expected command string length")
-		}
-
-		_, err := strconv.Atoi(p.tokens[p.pos].Value)
-		if err != nil {
-			return fmt.Errorf("invalid: wrong command length attribute %v", p.tokens[p.pos].Value)
-		}
-
-		return nil
-	},
-	stateCommandName: func(p *Parser) error {
-		if p.tokens[p.pos].Type != TokenString {
-			return fmt.Errorf("invalid: expected string as command")
-		}
-
-		return nil
-	},
-	stateArgLength: func(p *Parser) error {
-		if p.tokens[p.pos].Type != TokenBulkString {
-			return fmt.Errorf("invalid: expected command arg string length")
-		}
-
-		_, err := strconv.Atoi(p.tokens[p.pos].Value)
-		if err != nil {
-			return fmt.Errorf("invalid: wrong command arg length attribute %v", p.tokens[p.pos].Value)
-		}
-
-		return nil
-	},
-	stateArgValue: func(p *Parser) error {
-		if p.tokens[p.pos].Type != TokenString {
-			return fmt.Errorf("invalid: expected string as command arg")
-		}
-
-		return nil
-	},
+type ParserState struct {
+	validate  ParserStateChecker
+	process   ParserProcessor
+	nextState string
 }
 
-var processors map[string]ParserProcessor = map[string]ParserProcessor{
-	stateArrayHeader: func(p *Parser, c *Command) error {
-		arrayLength, err := strconv.Atoi(p.tokens[p.pos].Value)
-		if err != nil {
-			return fmt.Errorf("invalid: wrong array length attribute %v", p.tokens[p.pos].Value)
-		}
-
-		p.expectedLength = arrayLength
-		p.pos++
-		p.state = stateTransitions[p.state]
-		return nil
-	},
-	stateCommandLength: func(p *Parser, c *Command) error {
-		p.pos++
-		p.state = stateTransitions[p.state]
-		return nil
-	},
-	stateCommandName: func(p *Parser, c *Command) error {
-		command := p.tokens[p.pos].Value
-
-		if !slices.Contains(validCommands, command) {
-			return fmt.Errorf("invalid command: %v", command)
-		}
-
-		c.Name = strings.ToUpper(command)
-		p.pos++
-		p.state = stateTransitions[p.state]
-		p.actualLength++
-		return nil
-	},
-	stateArgLength: func(p *Parser, c *Command) error {
-		p.pos++
-		p.state = stateTransitions[p.state]
-		return nil
-	},
-	stateArgValue: func(p *Parser, c *Command) error {
-		c.Args = append(c.Args, p.tokens[p.pos].Value)
-		p.pos++
-		p.state = stateTransitions[p.state]
-		p.actualLength++
-		return nil
-	},
-}
-
-var stateTransitions map[string]string = map[string]string{
-	stateArrayHeader:   stateCommandLength,
-	stateCommandLength: stateCommandName,
-	stateCommandName:   stateArgLength,
-	stateArgLength:     stateArgValue,
-	stateArgValue:      stateArgLength,
-}
+var validCommands = []string{"ECHO", "PING", "SET"}
 
 type Parser struct {
 	tokens         []Token
-	pos            int // Current position in tokens
+	pos            int
 	state          string
 	expectedLength int
 	actualLength   int
+}
+
+var parserStates = map[string]ParserState{
+	stateArrayHeader: {
+		validate: func(p *Parser) error {
+			if p.tokens[p.pos].Type != TokenArray {
+				return fmt.Errorf("expected array token, got %s", p.tokens[p.pos].Type)
+			}
+			return nil
+		},
+		process: func(p *Parser, c *Command) error {
+			arrayLength, err := strconv.Atoi(p.tokens[p.pos].Value)
+			if err != nil {
+				return fmt.Errorf("invalid array length: %s", p.tokens[p.pos].Value)
+			}
+			p.expectedLength = arrayLength
+			return nil
+		},
+		nextState: stateCommandLength,
+	},
+	stateCommandLength: {
+		validate: func(p *Parser) error {
+			if p.tokens[p.pos].Type != TokenBulkString {
+				return fmt.Errorf("expected bulk string token for command length")
+			}
+			_, err := strconv.Atoi(p.tokens[p.pos].Value)
+			if err != nil {
+				return fmt.Errorf("invalid command length: %s", p.tokens[p.pos].Value)
+			}
+			return nil
+		},
+		process: func(p *Parser, c *Command) error {
+			return nil // Just validate length, actual command comes next
+		},
+		nextState: stateCommandName,
+	},
+	stateCommandName: {
+		validate: func(p *Parser) error {
+			if p.tokens[p.pos].Type != TokenString {
+				return fmt.Errorf("expected string token for command name")
+			}
+			if !slices.Contains(validCommands, strings.ToUpper(p.tokens[p.pos].Value)) {
+				return fmt.Errorf("invalid command: %s", p.tokens[p.pos].Value)
+			}
+			return nil
+		},
+		process: func(p *Parser, c *Command) error {
+			c.Name = strings.ToUpper(p.tokens[p.pos].Value)
+			p.actualLength++
+			return nil
+		},
+		nextState: stateArgLength,
+	},
+	stateArgLength: {
+		validate: func(p *Parser) error {
+			if p.tokens[p.pos].Type != TokenBulkString {
+				return fmt.Errorf("expected bulk string token for argument length")
+			}
+			_, err := strconv.Atoi(p.tokens[p.pos].Value)
+			if err != nil {
+				return fmt.Errorf("invalid argument length: %s", p.tokens[p.pos].Value)
+			}
+			return nil
+		},
+		process: func(p *Parser, c *Command) error {
+			return nil // Just validate length, actual argument comes next
+		},
+		nextState: stateArgValue,
+	},
+	stateArgValue: {
+		validate: func(p *Parser) error {
+			if p.tokens[p.pos].Type != TokenString {
+				return fmt.Errorf("expected string token for argument value")
+			}
+			return nil
+		},
+		process: func(p *Parser, c *Command) error {
+			c.Args = append(c.Args, p.tokens[p.pos].Value)
+			p.actualLength++
+			return nil
+		},
+		nextState: stateArgLength,
+	},
 }
 
 func (p *Parser) IsFinished() bool {
 	return p.pos >= len(p.tokens)
 }
 
+func (p *Parser) advanceToken() {
+	p.pos++
+
+	if p.pos < len(p.tokens) && p.tokens[p.pos].Type == TokenCRLF {
+		p.pos++
+	}
+}
+
 func (p *Parser) Parse() (*Command, error) {
 	command := &Command{}
+
 	for !p.IsFinished() {
+		state, exists := parserStates[p.state]
+		if !exists {
+			return nil, fmt.Errorf("invalid parser state: %s", p.state)
+		}
 
-		if err := checkers[p.state](p); err != nil {
+		if err := state.validate(p); err != nil {
 			return nil, err
 		}
 
-		if err := processors[p.state](p, command); err != nil {
+		if err := state.process(p, command); err != nil {
 			return nil, err
 		}
 
-		if p.tokens[p.pos].Type == TokenCRLF {
-			p.pos++
-		}
+		p.advanceToken()
+		p.state = state.nextState
 	}
 
 	if p.expectedLength != p.actualLength {
-		return nil, fmt.Errorf("wrong array length, expected: %d, actual: %d", p.expectedLength, p.actualLength)
+		return nil, fmt.Errorf("wrong array length, expected: %d, actual: %d",
+			p.expectedLength, p.actualLength)
 	}
 
 	return command, nil
 }
 
 func NewParser(tokens []Token) *Parser {
-	return &Parser{tokens: tokens, pos: 0, state: stateArrayHeader}
+	return &Parser{
+		tokens: tokens,
+		pos:    0,
+		state:  stateArrayHeader,
+	}
 }
